ce3f52888932837ac926aec1f6f65bbab9f296de
Tom Li <nklizhe@gmail.com>
2015-01-25 19:07:36 +0800

    Fix bugs in bootstrap.darwin.sh

--- bootstrap.darwin.sh
+++ bootstrap.darwin.sh
@@ -3,15 +3,19 @@
 which go || (
 	echo "installing go..."
 	brew update
-	brew install hg go gpm gvp
+	brew install hg go 
 )
+which gvp || brew install gvp
+which gpm || brew install gpm
+pkg-config --cflags portaudio-2.0 || brew install portaudio
+which gpg || brew install gpg
 
 gvp init
 source gvp in
 
 # install go tools
 go get golang.org/x/tools/cmd/vet
-go get golang.org.x/tools/oracle
+go get golang.org/x/tools/oracle
 go get golang.org/x/tools/cmd/cover
 go get golang.org/x/tools/cmd/goimports
 go get github.com/robertkrimen/godocdown
@@ -21,7 +25,7 @@ gpm install
 
 # cross-compiling toolchain
 go get github.com/mitchellh/gox
-gox -build-toolchain -os="darwin,linux,windows"
+gox -build-toolchain -os="darwin linux"
 
 # restore original gopath
 source gvp out
